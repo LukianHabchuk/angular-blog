ca316952a0795f22c6b1dc53709b5e15546715b1
Lukian Hrabchuk <lukian359@gmail.com>
2021-01-16 13:43:58 +0100

    firebase connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCY9kGOxFNv7N9gaU5eLVyS3bFAuMqjC0M'
 };
